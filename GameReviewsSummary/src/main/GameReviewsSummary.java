d4f5656f4685a900ecbde2f50047b87de5d0a0a9
MarceloNicaretta <marcelo.nicaretta@universo.univates.br>
2019-05-13 17:09:24 -0300

    Implementada leitura do arquivo game-reviews.csv

--- GameReviewsSummary/src/main/GameReviewsSummary.java
+++ GameReviewsSummary/src/main/GameReviewsSummary.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,22 +12,69 @@ import java.util.Map;
 public class GameReviewsSummary {
 
 	private static class GenreSummary {
-		private String genre;
 		private int reviews;
-		private List<Map<String, Integer>> scorePhrases = new ArrayList<Map<String,Integer>>();
+		private Map<String, Integer> scorePhrases = new HashMap<>();
 		private List<Integer> scores = new ArrayList<Integer>();
 	}
 
 	public static void main(String[] args) throws IOException {
+		Map<String,GenreSummary> summaries = new HashMap<>();
+		Integer highestScore = null;
+		Integer lowestScore = null;
+		String bestGame = null;
+		String worstGame = null;
+		
+		List<String> headers;
+		
 		try (BufferedReader br = new BufferedReader(new FileReader("game-reviews.csv"))) {
 		    String line = br.readLine();
 		    String[] str = line.split(";");
 		    
-		    while((line = br.readLine()) != null){
-	        str = line.split(";");
-		        for(int i = 0; i < str.length; i++){
-		            
-		        }
+		    headers = Arrays.asList(str);
+		    
+		    while((line = br.readLine()) != null) {
+		    	try {
+			    	str = line.split(";");
+			    	String game = str[headers.indexOf("title")];
+			        String genre = str[headers.indexOf("genre")];
+			        String scorePhrase = str[headers.indexOf("score_phrase")];
+			        Integer score = Integer.parseInt(str[headers.indexOf("score")]);
+			        
+			        // genre
+			        GenreSummary summary = summaries.get(genre);
+			        
+			        if (summary == null) {
+			        	summaries.put(genre, summary = new GenreSummary());
+			        }
+			        
+			        summary.reviews++;
+			        
+			        // score_phrase
+			        Integer phraseCount = summary.scorePhrases.get(scorePhrase);
+			        
+			        if (phraseCount == null) {
+			        	phraseCount = 0;
+			        }
+			        
+			        summary.scorePhrases.put(scorePhrase, ++phraseCount);
+			        
+			        // score
+			        summary.scores.add(score);
+			        
+			        // best game
+			        if (highestScore == null || highestScore < score) {
+			        	highestScore = score;
+			        	bestGame = game;
+			        }
+			        
+			        // worst game
+			        if (lowestScore == null || lowestScore > score) {
+			        	lowestScore = score;
+			        	worstGame = game;
+			        }
+		    	} catch (Exception e) {
+		    		e.printStackTrace();
+		    	}
 		    }
 		}
 	}
